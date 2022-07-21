be34df9971725c85ddb912017ac151fd41a59d93
manuetov <manuetov@hotmail.com>
2022-07-21 08:57:38 +0200

    a corregir falla partials

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const hbs = require('hbs');
+const path = require('path');
+const { config } = require('process');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + "/views/partials")
+
+// ...
+
+// Add the route handlers here:
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi) 
+    res.render('beers', {beersFromApi})
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi) 
+    res.render('randomBeer', {beersFromApi})
+  })
+  .catch(error => console.log(error));
+})
+
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
