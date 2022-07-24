f6b8e16474fa4c083830e6ce9e81152d14ca6bdc
manuetov <manuetov@hotmail.com>
2022-07-24 22:27:50 +0200

    corregido falta diseño

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
-const { config } = require('process');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -14,34 +13,63 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-hbs.registerPartials(__dirname + "/views/partials")
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('home');
+// **********************************************************************
+// ROUTE FOR GETTING ALL THE BEERS AND IT'S RENDERED ON "/BEERS"
+// **********************************************************************
+
+app.get("/", (req, res, next) => {
+  res.render("index.hbs")
+})
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers() // .getBeers() is the method provided by punkAPI
+    .then(responseFromDB => {
+      // console.log("Response is:",  responseFromDB);
+      // beers is the hbs file that's gonna be rendered, it comes from "views" folder
+      //  ^
+      //  |-------------- |          "beers" is the name of a variable we will use in hbs file
+      //                  |             |
+      res.render("beers.hbs", { beers: responseFromDB });
+    })
+    .catch(error => console.log(error));
 });
 
+// **********************************************************************
+// ROUTE FOR GETTING A RANDOM BEER AND IT'S RENDERED ON "/RANDOM-BEER"
+// **********************************************************************
 
-app.get('/beers', (req, res) => {
-  punkAPI.getBeers()
-  .then(beersFromApi => {
-    console.log('Beers from the database: ', beersFromApi) 
-    res.render('beers', {beersFromApi})
-  })
-  .catch(error => console.log(error));
-})
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom() // .getRandom() is the method provided by punkAPI
+    .then(responseFromApi => {
+      res.render('random-beer', { beers: responseFromApi });
 
-app.get('/random-beer', (req, res) => {
-  punkAPI.getRandom()
-  .then(beersFromApi => {
-    console.log('Beers from the database: ', beersFromApi) 
-    res.render('randomBeer', {beersFromApi})
-  })
-  .catch(error => console.log(error));
-})
+      // other way could be extracting this one beers from the array and
+      // sending it as object to the random-beers view
+      // but in that case we wouldn't be able to use partial, and we are aiming to use it later
+      // res.render('beers/random-beer', { beer: responseFromApi[0] });
+    })
+    .catch(error => console.log(error));
+});
 
+// **********************************************************************
+// ROUTE FOR GETTING DETAILS OF A SPECIFIC BEER AND IT'S RENDERED ON "/BEERS/someBeerIdGoesHere"
+// **********************************************************************
+app.get('/beers/:beerId', (req, res) => {
+  // console.log('params:', req.params);
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+  punkAPI
+    .getBeer(req.params.beerId)
+    .then(responseFromApi => {
+      //   console.log(responseFromApi);
+      res.render('beer-details.hbs', { beers: responseFromApi });
+    })
+    .catch(err => console.log(err));
+});
+
+app.listen(3700, () => console.log('🏃‍ on port 3800'));
